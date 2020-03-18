fbafeb9018b1c96e54d9f67342149a363050c88c
FelixRiegBaumhauer <riegbaumhauer@gmail.com>
2020-03-19 01:29:18 +0800

    clean up

--- marshal.h
+++ marshal.h
@@ -25,116 +25,11 @@ typedef enum message_type {
 } MessageType;
 
 
-/*
-typedef enum response_type {
-	Read = 0, 
-	Insert = 1, 
-	Monitor = 2, 
-	Shift = 3, 
-	Mode = 4
-} ResponseType;
-*/
-
 typedef enum success_type {
 	Good = 0,
 	Failure = 1,
 	OtherFailure = 2
 } SuccessType;
-
-/*
-typedef struct read_call{
-	char * filepath;
-	int offset;
-	int num;
-} Read_Call;
-
-typedef struct insert_call{
-	char * filepath;
-	int offset;
-	char * bytes;
-} Insert_Call;
-
-typedef struct monitor_call{
-	char * filepath;
-	int duration;
-} Monitor_Call;
-
-typedef struct shift_call{
-	char * filepath;
-} Shift_Call;
-
-typedef struct mode_call{
-	char * filepath;
-} Mode_Call;
-*/
-/*
-class ReadCall{
-public:
-	std::string filepath;
-	uint offset;
-	uint num;
-
-	ReadCall(std::string filepath, uint offset, uint num);
-	void print();
-};
-
-class InsertCall{
-public:
-	std::string filepath;
-	uint offset;
-	std::string bytes;
-
-	InsertCall(std::string filepath, uint offset, std::string bytes);
-	void print();
-};
-
-class MonitorCall{
-public:
-	std::string filepath;
-	uint duration;
-
-	MonitorCall(std::string filepath, uint duration);
-	void print();
-};
-
-class ShiftCall{
-public:
-	std::string filepath;
-
-	ShiftCall(std::string filepath);
-	void print();
-};
-
-class ModeCall{
-public:
-	std::string filepath;
-
-	ModeCall(std::string filepath);
-	void print();
-};
-*/
-
-/*
-class Call{
-public:
-	CallType callType;
-	std::vector<uint> intArgs;
-	std::vector<std::string> strArgs;
-
-	Call(CallType callType, std::vector<uint> intArgs, std::vector<std::string> strArgs);
-	void print();
-};
-
-class Response{
-public:
-	CallType respType;
-	SuccessType success;
-	std::string respString;
-
-	Response(CallType respType, SuccessType success, std::string respString);
-	void print();
-};
-*/
 
 
 class Message{
@@ -152,53 +47,8 @@ public:
 
 class Marshal{
 public:
-
-/*
-	unsigned char * marshalReadCall(ReadCall rc);
-	ReadCall unmarshalReadCall(unsigned char * buf_stream);
-
-	unsigned char * marshalInsertCall(InsertCall ic);
-	InsertCall unmarshalInsertCall(unsigned char * buf_stream);
-
-	unsigned char * marshalMonitorCall(MonitorCall mc);
-	MonitorCall unmarshalMonitorCall(unsigned char * buf_stream);
-
-	unsigned char * marshalShiftCall(ShiftCall sc);
-	ShiftCall unmarshalShiftCall(unsigned char * buf_stream);
-
-	unsigned char * marshalModeCall(ModeCall mc);
-	ModeCall unmarshalModeCall(unsigned char * buf_stream);
-*/
-
-/*
-	unsigned char * marshalCall(Call c);
-	Call unmarshalCall(unsigned char * buf_stream);
-
-	unsigned char * marshalResponse(Response r);
-	Response unmarshalResponse(unsigned char * buf_stream);
-*/
-
-/* 
-//THESE ARE THE GOOD ONES
-	unsigned char * marshalCall(Call c, uint * len);
-	Call unmarshalCall(unsigned char * buf_stream, uint * len);
-
-	unsigned char * marshalResponse(Response r, uint * len);
-	Response unmarshalResponse(unsigned char * buf_stream, uint * len);
-*/
-
 	unsigned char * marshalMessage(Message m, uint * len);
 	Message unmarshalMessage(unsigned char * buf_stream, uint * len);
-
-
-/*
-	uint marshalCall(Call c, unsigned char ** dest);
-	uint unmarshalCall(unsigned char * buf_stream, Call * dest);
-
-	uint marshalResponse(Response r, unsigned char ** dest);
-	uint unmarshalResponse(unsigned char * buf_stream, Response * dest);
-*/
-
 
 	uint packageStrings(std::vector<std::string> src, unsigned char * dest);
 	uint unpackageStrings(unsigned char * src, std::vector<std::string> * dest);
@@ -218,7 +68,6 @@ public:
 	uint stringLength(std::string s);
 	uint stringsLength(std::vector<std::string> strs);
 	uint intsLength(std::vector<uint> ints);
-
 
 };
 
